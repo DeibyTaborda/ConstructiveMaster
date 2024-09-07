36df52e1499a6962d13a6e31a87942a1de5e6a3d
Deiby Gerleyn Taborda Giraldo <gerleytaborda645@gmail.com>
2024-09-07 08:27:26 -0500

    Agregar diseño y mejorar manejo de lógica para la interfaz de gestión de trabajos

--- client/src/pages/administrador/Trabajos.jsx
+++ client/src/pages/administrador/Trabajos.jsx
@@ -1,77 +1,83 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import '../../assets/styles/forms.css';
 import MenuAdmin from "../../components/administrador/MenuAdmin.jsx";
 import TablaAdmin from "../../components/administrador/TablaAdmin.jsx";
 import TarjetaAgregarEntidad from "../../components/administrador/TarjetaAgregarEntidad.jsx";
+import ConfirmarAgregarProfesionalModal from "../../components/super-administrador/ConfirmarAgregarProfesionalModal.jsx";
+import FormEditarTrabajo from "../../components/administrador/FormEditarTrabajo.jsx";
 import useAxios from "../../services/api";
 import usePostRequest from "../../services/usePostRequest";
 import { usePutRequest } from "../../services/usePutRequest.js";
 import useDelete from "../../services/delete";
 import usePostRequestJson from "../../services/usePostRequestJson.js";
-import ConfirmarAgregarProfesionalModal from "../../components/super-administrador/ConfirmarAgregarProfesionalModal.jsx";
-import FormEditarTrabajo from "../../components/administrador/FormEditarTrabajo.jsx";
+import FormAgregarTrabajo from "../../components/super-administrador/FormAgregarTrabajo.jsx";
 
 function Trabajos() {
+    // Estados para gestionar trabajos
     const [idTrabajoSeleccionado, setIdTrabajoSeleccionado] = useState(null);
     const [profesionales, setProfesionales] = useState([]);
     const [errores, setErrores] = useState(null);
     const [respuestasExitosas, setRespuestasExitosas] = useState(null);
-    const [estado, setEstado] = useState({
-        estado: 'pendiente'
-    });
-    const [accionesTabla, setAccionestTabla] = useState('Acciones');
-    const [columnas, setColumnas] = useState(['id', 'nombre_cliente', 'nombre_profesional', 'fecha', 'hora', 'direccion', 'descripcion', 'valor', 'fecha_inicio', 'fecha_fin', 'estado']);
-    const {loading: loadingActualizar, error: errorActualizar, response: responseActualizar, sendPutRequest} = usePutRequest();
-    const {loading: loadingAgregar, error: errorAgregar, response: responseAgregar, postRequestJson} = usePostRequestJson('http://localhost:3001/trabajos/admin');
-    const {loading: loadingEliminar, error: errorEliminar, response: responseEliminar, eliminar} = useDelete(`http://localhost:3001/clientes/`);
+    const [estado, setEstado] = useState({ estado: 'pendiente' });
+    const [columnas, setColumnas] = useState([
+        'id', 'nombre_cliente', 'nombre_profesional', 'fecha', 'hora', 'direccion', 'descripcion', 'valor', 'fecha_inicio', 'fecha_fin', 'estado'
+    ]);
 
+    // Estados para ocultar y mostrar componentes
+    const [isOpenFormEditar, setOpenFormEditar] = useState(false);
+    const [isOpenConfirmarTrabajo, setOpenConfirmarTrabajo] = useState(false);
+    const [isOpenCancelarTrabajo, setIsOpenCancelarTrabajo] = useState(false);
+    const [isOpenFormAgregarTrabajo, setIsOpenFormAgregarTrabajo] = useState(false);
+
+    // Hooks personalizados para peticiones
+    const { loading: loadingActualizar, error: errorActualizar, response: responseActualizar, sendPutRequest } = usePutRequest();
+    const { loading: loadingObtener, error: errorObtener, response: responseObtener, postRequestJson : postRequestJsonObtener } = usePostRequestJson('http://localhost:3001/trabajos/admin');
+    const { loading: loadingAgregar, error: errorAgregar, response: responseAgregar, postRequestJson: postRequestJsonAgregar } = usePostRequestJson('http://localhost:3001/trabajos');
+    const { loading: loadingEliminar, error: errorEliminar, response: responseEliminar, eliminar } = useDelete('http://localhost:3001/clientes/');
+
+    // Manejo de cambio en el estado del trabajo
     const handleChange = async (event) => {
         const estadoSeleccionado = event.target.value;
-        setEstado({ estado: estadoSeleccionado }); // Actualizas correctamente
-        await postRequestJson({ estado: estadoSeleccionado }); // Usas el nuevo estado
+        setEstado({ estado: estadoSeleccionado });
+        console.log('Estado antes de enviar:', estado); 
+        await postRequestJsonObtener({ estado: estadoSeleccionado });
     };
-    
+
+    // Efectos para manejar actualizaciones en estado y respuestas
     useEffect(() => {
         if (estado) {
-            postRequestJson(estado); // Pasas el estado directamente
+            console.log('Llamando a postRequestJsonObtener con estado:', estado);
+            postRequestJsonObtener(estado);
         }
     }, [estado]);
     
+
     useEffect(() => {
-        if (responseAgregar) {
-            const response = responseAgregar.filter(profesional => profesional.estado !== 'cancelado' && profesional.estado !== 'confirmado');
-            
-            if (response.length > 0) {
-                if (!columnas.includes('Acciones')) {
-                    setColumnas([...columnas, 'Acciones']);
-                }
-            } else {
-                if (columnas.includes('Acciones')) {
-                    setColumnas(columnas.filter(col => col !== 'Acciones'));
-                }
+        if (responseObtener) {
+            const response = responseObtener.filter(profesional => profesional.estado !== 'cancelado' && profesional.estado !== 'confirmado');
+            if (response.length > 0 && !columnas.includes('Acciones')) {
+                setColumnas([...columnas, 'Acciones']);
+            } else if (response.length === 0 && columnas.includes('Acciones')) {
+                setColumnas(columnas.filter(col => col !== 'Acciones'));
             }
-            
-            setProfesionales(responseAgregar);
+            console.log(responseObtener)
+            setProfesionales(responseObtener);
         }
-    }, [responseAgregar]);
-
-
-    useEffect(() => {
-        if (errorAgregar) {
-            console.log(errorAgregar);
-        }
-    }, [errorAgregar])
+    }, [responseObtener]);
 
     useEffect(() => {
-        if (idTrabajoSeleccionado) {
-            console.log(idTrabajoSeleccionado)
-        }
-    }, [idTrabajoSeleccionado])
+        if (errorObtener) console.log(errorObtener);
+    }, [errorObtener]);
+
+    useEffect(() => {
+        if (idTrabajoSeleccionado) console.log(idTrabajoSeleccionado);
+    }, [idTrabajoSeleccionado]);
 
     useEffect(() => {
         if (errorActualizar) {
             setErrores(errorActualizar);
             setRespuestasExitosas('');
-        };
+        }
     }, [errorActualizar]);
 
     useEffect(() => {
@@ -79,40 +85,73 @@ function Trabajos() {
             setRespuestasExitosas(responseActualizar);
             setErrores('');
         }
-    }, [responseActualizar])
+    }, [responseActualizar]);
 
+    useEffect(() => {
+        if (errorAgregar) {
+            setErrores(errorAgregar);
+            setRespuestasExitosas('');
+        }
+    }, [errorAgregar])
+
+    useEffect(() => {
+        if (responseAgregar) {
+            setRespuestasExitosas(responseAgregar);
+            setErrores('');
+        }
+    }, [responseAgregar])
+
+    // Funciones para seleccionar, editar y crear trabajos y abrir Modales
     const seleccionarIdConfirmar = (id) => {
         setIdTrabajoSeleccionado(id);
+        setOpenConfirmarTrabajo(!isOpenConfirmarTrabajo);
     }
-
     const seleccionarIdCancelar = (id) => {
         setIdTrabajoSeleccionado(id);
+        setIsOpenCancelarTrabajo(!isOpenCancelarTrabajo);
     }
 
-    const editarEstadoTrabajo = async() => {
-        await sendPutRequest(`http://localhost:3001/trabajos/${idTrabajoSeleccionado}`, {estado: 'confirmado'});
-        await postRequestJson(estado);
-    }
+    const editarEstadoTrabajo = async () => {
+        await sendPutRequest(`http://localhost:3001/trabajos/${idTrabajoSeleccionado}`, { estado: 'confirmado' });
+        await postRequestJsonObtener(estado);
+    };
 
-    const cancelarTrabajo = async() => {
-        await sendPutRequest(`http://localhost:3001/trabajos/${idTrabajoSeleccionado}`, {estado: 'cancelado'});
-        await postRequestJson(estado);
-    }
+    const cancelarTrabajo = async () => {
+        await sendPutRequest(`http://localhost:3001/trabajos/${idTrabajoSeleccionado}`, { estado: 'cancelado' });
+        await postRequestJsonObtener(estado);
+    };
 
-    const editarTrabajo = async(data) => {
+    const editarTrabajo = async (data) => {
         await sendPutRequest(`http://localhost:3001/trabajos/${idTrabajoSeleccionado}`, profesionales);
-        await postRequestJson(estado);
+        await postRequestJsonObtener(estado);
+    };
+
+    const agregarTrabajo = async(data) => {
+        await postRequestJsonAgregar(data);
+        await postRequestJsonObtener(estado);
     }
 
+    // Datos del trabajo seleccionado
     const datosTrabajoSeleccionado = profesionales.find(trabajo => trabajo.id === idTrabajoSeleccionado);
 
+    // Funciones para ocultar modales y formulario editar
+    const cerrarModalConfirmarTrabajo = () => setOpenConfirmarTrabajo(false);
+    const cerrarModalCancelarTrabajo = () => setIsOpenCancelarTrabajo(false);
+    const cerrarFormEditarTrabajo = () => setOpenFormEditar(false);
+    const cerrarFormAgregarTrabajo = () => setIsOpenFormAgregarTrabajo(false);
+
+    // Función para renderizar el formulario de editar trabajo
+    const abrirFormEditarTrabajo = () => setOpenFormEditar(!isOpenFormEditar);
+    const abrirFormAgregarTrabajo = () => setIsOpenFormAgregarTrabajo(!isOpenFormAgregarTrabajo);
+
+    // Render del componente
     return (
-        <>
-             <div className="container-menu-table-clientes">
-                <MenuAdmin/>
-                <div className="container-table-clientes">
-                    <div className="subcontenedor-tabla-clientes">
-                    <select name="estado"  onChange={handleChange}>
+        <div className="container-menu-table-clientes">
+            <MenuAdmin />
+            <div className="container-table-clientes">
+                <div className="subcontenedor-tabla-clientes">
+                    {/* Selector para cambiar el estado del trabajo */}
+                    <select name="estado" onChange={handleChange}>
                         <option value="pendiente">Pendiente</option>
                         <option value="confirmado">Confirmado</option>
                         <option value="en_progreso">En Progreso</option>
@@ -120,23 +159,50 @@ function Trabajos() {
                         <option value="cancelado">Cancelado</option>
                         <option value="rechazado">Rechazado</option>
                     </select>
-                        <TablaAdmin data={profesionales} columns={columnas} title={'Trabajos'} tableId={'trabajos'} onClickEdit={seleccionarIdConfirmar} onClick={seleccionarIdCancelar}/>
-                    </div>
-                    <TarjetaAgregarEntidad cadena={'Añadir nuevo cliente'}/>
-                    <ConfirmarAgregarProfesionalModal onClick={editarEstadoTrabajo}/>
-                    <ConfirmarAgregarProfesionalModal onClick={cancelarTrabajo}/>
-                    <FormEditarTrabajo datos={datosTrabajoSeleccionado} solicitudPUT={editarTrabajo}/>
-                    {typeof errores === 'object' && errores !== null ? Object.values(errores)
-                        .map((error, index) => (
-                            <p key={index}>{error}</p>
-                        )) : (
-                            <p>{errores}</p>
-                    )}
-                    {respuestasExitosas ? <p>{respuestasExitosas}</p> : ''}
-                    
+
+                    {/* Tabla con los trabajos */}
+                    <TablaAdmin
+                        data={profesionales}
+                        columns={columnas}
+                        title="Trabajos"
+                        tableId="trabajos"
+                        onClickEdit={seleccionarIdConfirmar}
+                        onClick={seleccionarIdCancelar}
+                    />
                 </div>
+
+                {/* Componentes para agregar, confirmar o cancelar trabajo */}
+                <TarjetaAgregarEntidad cadena="Añadir nuevo cliente" onClick={abrirFormAgregarTrabajo}/>
+                {isOpenConfirmarTrabajo && (
+                    <div className="contenedor-con-opacidad">
+                        <ConfirmarAgregarProfesionalModal onClick={editarEstadoTrabajo} onClickCancelar={cerrarModalConfirmarTrabajo}/>
+                    </div>
+                )}
+                {isOpenCancelarTrabajo && (
+                    <div className="contenedor-con-opacidad">
+                        <ConfirmarAgregarProfesionalModal onClick={cancelarTrabajo} onClickCancelar={cerrarModalCancelarTrabajo} />
+                    </div>
+                )}
+                {isOpenFormAgregarTrabajo && (
+                    <div className="contenedor-con-opacidad">
+                        <FormAgregarTrabajo solicitudPOST={agregarTrabajo} onClick={cerrarFormAgregarTrabajo}/>
+                    </div>
+                )}
+                {isOpenFormEditar && (
+                    <div className="contenedor-con-opacidad">
+                        <FormEditarTrabajo datos={datosTrabajoSeleccionado} solicitudPUT={editarTrabajo} onClick={cerrarFormEditarTrabajo}/>
+                    </div>
+                )}
+
+                {/* Mostrar errores o respuestas exitosas */}
+                {typeof errores === 'object' && errores !== null ? (
+                    Object.values(errores).map((error, index) => <p key={index}>{error}</p>)
+                ) : (
+                    <p>{errores}</p>
+                )}
+                {/* {respuestasExitosas && <p>{respuestasExitosas}</p>} */}
             </div>
-        </>
+        </div>
     );
 }
 
